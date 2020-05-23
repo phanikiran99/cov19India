133c1197281db3df1f4368a8cf264895514f2da9
Phanikiran S <phanikiran99@gmail.com>
2020-05-23 21:39:26 +0530

    updated with geoplots

--- app.py
+++ app.py
@@ -2,90 +2,33 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output, State
-#plotly
-import plotly.graph_objects as go
-#data
-import pandas as pd
-import numpy as np
-#api
-import requests
+from dash.dependencies import Input, Output
+import plots
+import geo
+import base64
 
-# get the data
-api = 'https://api.covid19india.org/data.json'
-resp = requests.get(api)
 
-caseTimeSeries = pd.DataFrame(resp.json()['cases_time_series'])  # timeseries data
-stateWise = pd.DataFrame(resp.json()['statewise']) # statewise
-tested = pd.DataFrame(resp.json()['tested']) # test statistics
-
-#total statistics, Daily Cases trends 
-fig = go.Figure()
-
-fig.add_trace(go.Scatter(x=caseTimeSeries['date'], y=caseTimeSeries['totalconfirmed'], name='Total Confirmed'))
-fig.add_trace(go.Scatter(x=caseTimeSeries['date'], y=caseTimeSeries['totalrecovered'], name='Total Recovered'))
-fig.add_trace(go.Scatter(x=caseTimeSeries['date'], y=caseTimeSeries['totalconfirmed'].map(int)-caseTimeSeries['totalrecovered'].map(int), name='Total Active'))
-fig.add_trace(go.Scatter(x=caseTimeSeries['date'], y=caseTimeSeries['totaldeceased'], name='Total Deceased'))
-fig.add_trace(go.Bar(x=caseTimeSeries['date'], y=caseTimeSeries['dailyconfirmed'], name='Daily Confirmed'))
-fig.add_trace(go.Bar(x=caseTimeSeries['date'], y=caseTimeSeries['dailydeceased'], name='Daily Deceased'))
-fig.add_trace(go.Bar(x=caseTimeSeries['date'], y=caseTimeSeries['dailyrecovered'], name='Daily Recovered'))
-
-fig.update_layout(barmode='stack')
-
-#Doubling Rate
-pctChange = pd.DataFrame({'date':caseTimeSeries['date'],'totalconfirmed':caseTimeSeries['totalconfirmed'],'pct_change':caseTimeSeries['totalconfirmed'].map(int).pct_change().mul(100).round(2)})
-pctChange['cumul_pct'] = pctChange['pct_change'].cumsum()
-p=1
-rates = []
-for i in range(100):
-  if p > caseTimeSeries['totalconfirmed'].map(int).max():
-    break
-  else:
-    rates.append(p)
-    p = p *2
-
-def doubling_rate(lst):
-  # rates = rates
-  tf = []
-  for val in lst:
-    try:
-      if val >= rates[0]:
-        # print (val, rates[0])
-        rates.pop(0)
-        
-        tf.append(True)
-      else:
-        tf.append(False)
-    except IndexError:
-      # print (rates,len(rates))
-      # print ('exception')
-      tf.append(False)
-  return tf
-
-pctChange['double'] = doubling_rate(pctChange['totalconfirmed'].map(int))
-pctChangenoDup = pctChange.drop_duplicates('cumul_pct',keep='last')
-pctChangenoDup['double'] = doubling_rate(pctChange.drop_duplicates('cumul_pct',keep='last')['cumul_pct'])
-doublingData = pctChange[pctChange['double']]
-doublingData['doubleRate'] = doublingData.reset_index()['index'].diff().fillna(0).to_list()
-
-fig2 = go.Figure()
-fig2.add_trace(go.Bar(x=doublingData['date'],y=doublingData['doubleRate'], name='DoublingRate of Cases'))
-fig2.add_trace(go.Scatter(x=doublingData['date'],y=doublingData['doubleRate'], name='DoublingRate of Cases'))
-fig2.add_trace(go.Scatter(x=doublingData['date'],y=doublingData['totalconfirmed'].map(int).map(np.log), name='Total Cases LogScale'))
-fig2.update_layout(title='Doubling Rate of Cases in India ' + str(doublingData['doubleRate'].iloc[-1]))
-
+fig  = plots.fig  # trend line
+fig2 = plots.fig2 # doubling rate
 
 url_bar_and_content_div = html.Div([
     dcc.Location(id='url', refresh=False),
     html.Div(id='page-content')
 ])
-    
+
+rec_filename = 'recovery.png' # replace with your own image
+
+encoded_image_rec = base64.b64encode(open(rec_filename, 'rb').read())
+
+
+act_filename = 'active.png' # replace with your own image
+encoded_image_act = base64.b64encode(open(act_filename, 'rb').read())    
     
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
-app.title="Cov19India"
+app.title="Phani-Writes"
 
 layout_index = html.Div([
     dcc.Link('Navigate to "covid19India"', href='/covid19'),
@@ -105,6 +48,8 @@ covid_layout= html.Div(children=[
         id='Cov2',
         figure=fig2
     ),
+    html.Img(src='data:image/png;base64,{}'.format(encoded_image_act.decode())),
+    html.Img(src='data:image/png;base64,{}'.format(encoded_image_rec.decode())),
     html.A('Code on Github', href='https://github.com/phanikiran99/cov19India'),
     html.Br(),
     html.A('Data Source', href='covid19india.org'),
---
+++ plots.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+#plotly
+import plotly.graph_objects as go
+#data
+import pandas as pd
+import numpy as np
+#api
+import requests
+
+# get the data
+api = 'https://api.covid19india.org/data.json'
+resp = requests.get(api)
+
+caseTimeSeries = pd.DataFrame(resp.json()['cases_time_series'])  # timeseries data
+stateWise = pd.DataFrame(resp.json()['statewise']) # statewise
+tested = pd.DataFrame(resp.json()['tested']) # test statistics
+
+#total statistics, Daily Cases trends 
+fig = go.Figure()
+
+fig.add_trace(go.Scatter(x=caseTimeSeries['date'], y=caseTimeSeries['totalconfirmed'], name='Total Confirmed'))
+fig.add_trace(go.Scatter(x=caseTimeSeries['date'], y=caseTimeSeries['totalrecovered'], name='Total Recovered'))
+fig.add_trace(go.Scatter(x=caseTimeSeries['date'], y=caseTimeSeries['totalconfirmed'].map(int)-caseTimeSeries['totalrecovered'].map(int), name='Total Active'))
+fig.add_trace(go.Scatter(x=caseTimeSeries['date'], y=caseTimeSeries['totaldeceased'], name='Total Deceased'))
+fig.add_trace(go.Bar(x=caseTimeSeries['date'], y=caseTimeSeries['dailyconfirmed'], name='Daily Confirmed'))
+fig.add_trace(go.Bar(x=caseTimeSeries['date'], y=caseTimeSeries['dailydeceased'], name='Daily Deceased'))
+fig.add_trace(go.Bar(x=caseTimeSeries['date'], y=caseTimeSeries['dailyrecovered'], name='Daily Recovered'))
+
+fig.update_layout(barmode='stack')
+
+#Doubling Rate
+pctChange = pd.DataFrame({'date':caseTimeSeries['date'],'totalconfirmed':caseTimeSeries['totalconfirmed'],'pct_change':caseTimeSeries['totalconfirmed'].map(int).pct_change().mul(100).round(2)})
+pctChange['cumul_pct'] = pctChange['pct_change'].cumsum()
+p=1
+rates = []
+for i in range(100):
+  if p > caseTimeSeries['totalconfirmed'].map(int).max():
+    break
+  else:
+    rates.append(p)
+    p = p *2
+
+def doubling_rate(lst):
+  # rates = rates
+  tf = []
+  for val in lst:
+    try:
+      if val >= rates[0]:
+        # print (val, rates[0])
+        rates.pop(0)
+        
+        tf.append(True)
+      else:
+        tf.append(False)
+    except IndexError:
+      # print (rates,len(rates))
+      # print ('exception')
+      tf.append(False)
+  return tf
+
+pctChange['double'] = doubling_rate(pctChange['totalconfirmed'].map(int))
+pctChangenoDup = pctChange.drop_duplicates('cumul_pct',keep='last')
+pctChangenoDup['double'] = doubling_rate(pctChange.drop_duplicates('cumul_pct',keep='last')['cumul_pct'])
+doublingData = pctChange[pctChange['double']]
+doublingData['doubleRate'] = doublingData.reset_index()['index'].diff().fillna(0).to_list()
+
+fig2 = go.Figure()
+fig2.add_trace(go.Bar(x=doublingData['date'],y=doublingData['doubleRate'], name='DoublingRate of Cases'))
+fig2.add_trace(go.Scatter(x=doublingData['date'],y=doublingData['doubleRate'], name='DoublingRate of Cases'))
+fig2.add_trace(go.Scatter(x=doublingData['date'],y=doublingData['totalconfirmed'].map(int).map(np.log), name='Total Cases LogScale'))
+fig2.update_layout(title='Doubling Rate of Cases in India ' + str(doublingData['doubleRate'].iloc[-1]))
+
